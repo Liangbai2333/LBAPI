dbba8507837297bb4b32c7a9b8abddd7e7403efc
Liangbai2333 <1739566851@qq.com>
2024-08-26 17:32:39 +0800

    opt

--- src/main/kotlin/site/liangbai/lbapi/config/ConfigManager.kt
+++ src/main/kotlin/site/liangbai/lbapi/config/ConfigManager.kt
@@ -4,9 +4,15 @@ import taboolib.module.configuration.ConfigFile
 
 object ConfigManager {
     val binds = mutableMapOf<Any, ConfigFile>()
+    val flushCache = mutableListOf<Any>()
 
     fun Any.bindTo(configFile: ConfigFile) {
         binds[this] = configFile
+    }
+
+    fun Any.flushConf() {
+        binds[this]!!.reload()
+        flushCache.add(this)
     }
 
     fun getBind(obj: Any): ConfigFile {
--- src/main/kotlin/site/liangbai/lbapi/config/delegate/ConfigNode.kt
+++ src/main/kotlin/site/liangbai/lbapi/config/delegate/ConfigNode.kt
@@ -9,16 +9,29 @@ import kotlin.reflect.KProperty
 
 @Suppress("UNCHECKED_CAST")
 class ConfigNode<T>(private var node: String = "", private val mapper: ConfigMapper<T>? = null) : ReadWriteProperty<Any?, T?>  {
+    private var cached: T? = null
+
     override fun getValue(thisRef: Any?, property: KProperty<*>): T? {
         if (thisRef == null) throw IllegalArgumentException("bind access")
 
+        if (cached != null) {
+            if (thisRef in ConfigManager.flushCache) {
+                cached = null
+                ConfigManager.flushCache.remove(thisRef)
+            } else {
+                return cached
+            }
+        }
         val n = node.ifEmpty { property.name }
         val conf = ConfigManager.getBind(thisRef)
         val get = conf[n] ?: return null
-        if (mapper != null) {
-            return mapper.map(get as ConfigurationSection)
+        val returnValue: T? = if (mapper != null) {
+            mapper.map(get as ConfigurationSection)
+        } else {
+            get as T
         }
-        return get as T
+
+        return returnValue.also { cached = it }
     }
 
     override fun setValue(thisRef: Any?, property: KProperty<*>, value: T?) {
